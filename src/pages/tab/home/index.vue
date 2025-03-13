454884532588d8ac34b7a171c57e4bf431fd333d
qyh <qyh@knownsec.com>
2025-03-13 16:30:25 +0800

    chore: init project

---
+++ src/pages/tab/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="flex flex-col items-center justify-center">
+    <view class="flex justify-center">
+      <text class="font-size-36rpx color-gray-700">
+        行情页面
+      </text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/store';
+
+// const title = ref<string>();
+// title.value = import.meta.env.VITE_APP_TITLE;
+const userStore = useUserStore();
+console.log('userStore.user_name', userStore.user_name);
+</script>
---
+++ src/pages/tab/withdrawal/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="flex flex-col items-center justify-center">
+    <view class="flex justify-center">
+      <text class="font-size-36rpx color-gray-700">
+        提币页面
+      </text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/store';
+
+const userStore = useUserStore();
+console.log('userStore.user_name', userStore.user_name);
+</script>
